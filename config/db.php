4a91e39b22ea400e0a5c8990412a1fadaafd1c06
yuriy.chernyavskyi <yuriy.chernyavskyi@4k.com.ua>
2021-05-05 10:40:20 +0300

    change

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=aidating',
-    'username' => 'root',
-    'password' => '111',
+    'username' => 'Username',
+    'password' => 'Password',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
